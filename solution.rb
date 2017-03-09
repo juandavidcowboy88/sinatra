89f1ca10a168796a6f065407beecef0181739174
juand <ing.juandavid.alvarez@gmail.com>
2017-03-08 21:07:38 -0500

    1

--- solution.rb
+++ solution.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
-
-get '/' do
-erb :home
+class Numbero
+ attr_accessor  :var1
+  def initialize
+ @var1 = 0
   end
+  def add
+  @var1 += 1
+  end
+end
+
+obj = Numbero.new
+get '/' do
+ @number = obj.var1
+  erb :home
+  end
+
+
+ post '/' do
+  obj.add()
+ redirect '/'
+ end
